e586c8685348c15577417233462fd5f995443fa5
YuriyGonsor <yurii.gonsor@logivations.com>
2023-12-19 00:57:31 +0200

    preprocessed trip_data

--- workers/FareWorker.py
+++ workers/FareWorker.py
@@ -1,22 +1,22 @@
 from pyspark.sql import SparkSession
 import pyspark.sql.types as t
-
+from workers.Aliases import *
 import config
 
 
 class FareWorker:
     fare_schema = t.StructType([
-        t.StructField("medallion", t.StringType(), False),
-        t.StructField("hack_license", t.StringType(), False),
-        t.StructField("vendor_id", t.StringType(), False),
-        t.StructField("pickup_datetime", t.TimestampType(), False),
-        t.StructField("payment_type", t.StringType(), False),
-        t.StructField("fare_amount", t.FloatType(), False),
-        t.StructField("surcharge", t.FloatType(), False),
-        t.StructField("mta_tax", t.FloatType(), False),
-        t.StructField("tip_amount", t.FloatType(), False),
-        t.StructField("tolls_amount", t.FloatType(), False),
-        t.StructField("total_amount", t.FloatType(), False),
+        t.StructField(medallion, t.StringType(), False),
+        t.StructField(hack_license, t.StringType(), False),
+        t.StructField(vendor_id, t.StringType(), False),
+        t.StructField(pickup_datetime, t.TimestampType(), False),
+        t.StructField(payment_type, t.StringType(), False),
+        t.StructField(fare_amount, t.FloatType(), False),
+        t.StructField(surcharge, t.FloatType(), False),
+        t.StructField(mta_tax, t.FloatType(), False),
+        t.StructField(tip_amount, t.FloatType(), False),
+        t.StructField(tolls_amount, t.FloatType(), False),
+        t.StructField(total_amount, t.FloatType(), False),
     ])
 
     def __init__(self, spark: SparkSession):
@@ -24,6 +24,3 @@ class FareWorker:
 
     def show(self):
         self.df.show()
-
-    def count(self) -> int:
-        self.df.count()
--- workers/TripWorker.py
+++ workers/TripWorker.py
@@ -1,35 +1,44 @@
 from pyspark.sql import SparkSession
 import pyspark.sql.types as t
+from pyspark.sql.functions import col
 
+from workers.Aliases import *
 import config
+from pyspark.sql import DataFrame
 
 
 class TripWorker:
     trip_schema = t.StructType([
-        t.StructField("medallion", t.StringType(), False),
-        t.StructField("hack_license", t.StringType(), False),
-        t.StructField("vendor_id", t.StringType(), False),
-        t.StructField("rate_code", t.IntegerType(), False),
-        t.StructField("store_and_fwd_flag", t.BooleanType(), False),
-        t.StructField("pickup_datetime", t.TimestampType(), False),
-        t.StructField("dropoff_datetime", t.TimestampType(), False),
-        t.StructField("passenger_count", t.IntegerType(), False),
-        t.StructField("trip_time_in_secs", t.IntegerType(), False),
-        t.StructField("trip_distance", t.FloatType(), False),
-        t.StructField("pickup_longitude", t.FloatType(), False),
-        t.StructField("pickup_latitude", t.FloatType(), False),
-        t.StructField("dropoff_longitude", t.FloatType(), False),
-        t.StructField("dropoff_latitude", t.FloatType(), False),
+        t.StructField(medallion, t.StringType(), False),
+        t.StructField(hack_license, t.StringType(), False),
+        t.StructField(vendor_id, t.StringType(), False),
+        t.StructField(rate_code, t.IntegerType(), False),
+        t.StructField(store_and_fwd_flag, t.StringType(), False),
+        t.StructField(pickup_datetime, t.TimestampType(), False),
+        t.StructField(dropoff_datetime, t.TimestampType(), False),
+        t.StructField(passenger_count, t.IntegerType(), False),
+        t.StructField(trip_time_in_secs, t.IntegerType(), False),
+        t.StructField(trip_distance, t.FloatType(), False),
+        t.StructField(pickup_longitude, t.FloatType(), False),
+        t.StructField(pickup_latitude, t.FloatType(), False),
+        t.StructField(dropoff_longitude, t.FloatType(), False),
+        t.StructField(dropoff_latitude, t.FloatType(), False),
     ])
 
-    def __init__(self, spark: SparkSession):
-        self.df = spark.read.csv(config.trip_dataset, self.trip_schema, header=True, nullValue='null')
-        self.df = self.df
+    preprocess_filter_condition = ((col(passenger_count) > 0) & (col(passenger_count) <= 9)
+                                   & (col(trip_distance) > 0) & (col(trip_distance) < 70_000)
+                                   & (col(trip_time_in_secs) > 0) & (col(trip_time_in_secs) < 60_000))
 
-    def show(self):
-        self.df.show()
+    def __init__(self, spark: SparkSession):
+        self.df: DataFrame = (spark.read
+                              .csv(config.trip_dataset, self.trip_schema, header=True, nullValue='null')
+                              .filter(self.preprocess_filter_condition))
+
+    def show(self, n=None):
+        self.df.show(n=n)
 
     def count(self) -> int:
         return self.df.count()
 
-
+    def schema(self):
+        self.df.printSchema()
