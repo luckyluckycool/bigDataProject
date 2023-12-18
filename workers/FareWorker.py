24a21ef5ab1d6477e2d968849d3c982a9f536597
YuriyGonsor <yurii.gonsor@logivations.com>
2023-12-19 01:31:55 +0200

    preprocessed fare_data

--- workers/FareWorker.py
+++ workers/FareWorker.py
@@ -1,5 +1,7 @@
 from pyspark.sql import SparkSession
 import pyspark.sql.types as t
+from pyspark.sql.functions import col
+
 from workers.Aliases import *
 import config
 
@@ -19,8 +21,19 @@ class FareWorker:
         t.StructField(total_amount, t.FloatType(), False),
     ])
 
+    preprocess_filter_condition = (
+            (col(fare_amount) > 0)
+            & (col(surcharge) > 0)
+            & (col(mta_tax) > 0)
+            & (col(tip_amount) > 0)
+            & (col(tolls_amount) > 0)
+            & (col(total_amount) > 0)
+    )
+
     def __init__(self, spark: SparkSession):
-        self.df = spark.read.csv(config.fare_dataset, self.fare_schema, header=True, nullValue='null')
+        self.df = (spark.read
+                   .csv(config.fare_dataset, self.fare_schema, header=True, nullValue='null')
+                   .filter(self.preprocess_filter_condition))
 
     def show(self):
         self.df.show()
