c5d1194c164a56dc3ab64f98920045fe34128a9d
YuriyGonsor <yurii.gonsor@logivations.com>
2023-12-20 04:27:11 +0200

    queries

--- config.py
+++ config.py
@@ -1,2 +1,18 @@
+from pyspark.sql import DataFrame, SparkSession
+from pyspark import SparkConf
+
 trip_dataset = 'dataset/trip_data'
 fare_dataset = 'dataset/trip_fare'
+output_folder = 'output'
+
+
+def write_to_csv(df: DataFrame, filename: str) -> None:
+    df.write.csv(output_folder + '/' + filename + '.csv', header=True, mode='overwrite')
+
+
+def init_spark() -> SparkSession:
+    return (SparkSession.builder
+            .master("local")
+            .appName("Python")
+            .config(conf=SparkConf())
+            .getOrCreate())
